728896e878c7a0d8e2664453fb0a88c1aeab96eb
Aneel Singh <ajsingh1012@gmail.com>
2021-05-26 21:10:37 -0600

    Initial commit

--- dev-profile/src/App.js
+++ dev-profile/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 function App() {
+  $.ajax({
+    url: "https://api.github.com/users/blackmiaool/repos",
+    jsonp: true,
+    method: "GET",
+    dataType: "json",
+    success: function(res) {
+      console.log(res)
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
