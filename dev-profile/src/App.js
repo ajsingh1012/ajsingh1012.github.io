bf9c9b73817af1aaa69a815d377117e9c3272b70
Aneel Singh <ajsingh1012@gmail.com>
2021-06-04 20:19:54 -0600

    Initial commit

--- dev-profile/src/App.js
+++ dev-profile/src/App.js
@@ -10,12 +10,12 @@ function App() {
     dataType: "json",
     success: function(res) {
       console.log(res);
-      res.array.forEach(element => {
+      /*res.array.forEach(element => {
         console.log(element);
         if(element.has_pages) {
           console.log(document.URL + element.name);
         }
-      });
+      });*/
     }
   });
 
